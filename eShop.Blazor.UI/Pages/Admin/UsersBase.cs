41b334bc40482e60209b236946c3736b5f252475
PanagiotisGkan <pgantis@softone.gr>
2022-02-15 17:39:42 +0200

    A

---
+++ eShop.Blazor.UI/Pages/Admin/UsersBase.cs
@@ -0,0 +1,22 @@
+﻿using eShop.Blazor.UI.Dto_Model;
+using eShop.Blazor.UI.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShop.Blazor.UI.Pages.Admin
+{
+    public class UsersBase : ComponentBase
+    {
+        [Inject]
+        public IUserService UserService { get; set; }
+        public IEnumerable<User> Users { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            Users = (await UserService.GetUsersAsync()).ToList();
+        }
+    }
+}
